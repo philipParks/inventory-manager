df180c9701bc2ef6be03e29abee0d16cbe9f9aac
philipParks <philiprparks@gmail.comNoneqs>
2025-04-16 14:48:39 -0400

    removing unused imports

--- src/main/java/parks/inventorymanager/controller/DistributorViewController.java
+++ src/main/java/parks/inventorymanager/controller/DistributorViewController.java
@@ -2,13 +2,8 @@ package parks.inventorymanager.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.stage.Stage;
 import parks.inventorymanager.dao.DistributorDAO;
 import parks.inventorymanager.util.HelpMethods;
 
--- src/main/java/parks/inventorymanager/controller/WorkstationViewController.java
+++ src/main/java/parks/inventorymanager/controller/WorkstationViewController.java
@@ -1,16 +1,11 @@
 package parks.inventorymanager.controller;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 import parks.inventorymanager.dao.WorkstationDAO;
 import parks.inventorymanager.util.HelpMethods;
 
